c32f9a716d001ac293c85bd78065400da7fc6d6c
cedric thonney <c.thonney@gmail.com>
2021-10-16 16:19:11 +0400

    XXXX

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :spots
+  has_one_attached :photo
+
 end
