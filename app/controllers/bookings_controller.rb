288b12a3c33a5172dd6cfdc6781b2db4550d3f6a
Remi Dumas <rdumas.remi@gmail.com>
2021-10-16 14:14:06 +0400

    booking controller & view mis en pause

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,49 +1,15 @@
 class BookingsController < ApplicationController
-  def index
-    @bookings = Booking.all
-  end
-
-  def show
-    @spot = Spot.find(params[:id])
-  end
-
-  def new
+   def create
+    @booking = Booking.new(booking_params)
     @spot = Spot.find(params[:spot_id])
-    @booking = Booking.new
-  end
-
-  def create
-    @spot = Spot.new(spot_params)
-    @spot.user_id=current_user.id
-    @spot.save
-
-    # no need for app/views/restaurants/create.html.erb
+    @booking.spot = @spot
+    @booking.save
     redirect_to spot_path(@spot)
-  end
-
-  def edit
-    @spot = Spot.find(params[:id])
-  end
-
-  def update
-    @spot = Spot.find(params[:id])
-    @spot.update(spot_params)
-
-    # no need for app/views/restaurants/update.html.erb
-    redirect_to spot_path(@spot)
-  end
-
-  def destroy
-    @spot = Spot.find(params[:id])
-    @spot.destroy
-
-    # no need for app/views/restaurants/destroy.html.erb
-    redirect_to spots_path
   end
 
   private
 
-  def spot_params
-    params.require(:spot).permit(:address, :title, :description, :capacity, :price)
+  def booking_params
+    params.require(:review).permit(:content)
   end
 end
