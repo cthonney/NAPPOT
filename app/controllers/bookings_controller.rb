d8b6e7e697f7cd3b62ac07ee52534709756bd020
cedric thonney <c.thonney@gmail.com>
2021-10-19 18:59:28 +0400

    bookings-methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,18 @@
 class BookingsController < ApplicationController
-   def create
+
+  def index
+    @bookings = current_user.bookings
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
     @booking = Booking.new(booking_params)
     @spot = Spot.find(params[:spot_id])
     @booking.spot = @spot
@@ -7,9 +20,26 @@ class BookingsController < ApplicationController
     redirect_to spot_path(@spot)
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking[:id])
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def booking_params
-    params.require(:review).permit(:content)
+    params.require(:booking).permit(:start_time, :end_time, :user_id, :spot_id, :price)
   end
 end
+
