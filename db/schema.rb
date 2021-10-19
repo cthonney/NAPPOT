dad0108e1ac4195c1ac71b1261765230c14353c7
Remi Dumas <rdumas.remi@gmail.com>
2021-10-19 19:59:01 +0400

    ajout env file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_084541) do
+ActiveRecord::Schema.define(version: 2021_10_19_151302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_10_16_084541) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "booking_price"
     t.index ["spot_id"], name: "index_bookings_on_spot_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
